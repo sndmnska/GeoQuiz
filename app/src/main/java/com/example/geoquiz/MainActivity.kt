def018028d2d09f438c567f69afe86825fec8b4a
dan <email@email.com>
2023-02-08 16:05:53 -0600

    Added CheatActivity.kt, et al

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.geoquiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -7,22 +8,28 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import androidx.lifecycle.ViewModelProvider
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.ActivityResult
 import androidx.lifecycle.ViewModelProviders
+
 
 private const val TAG = "MainActivity"
 private const val KEY_INDEX = "index"
+private const val REQUEST_CODE_CHEAT = 0
+
+
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var nextButton: Button
     private lateinit var questionTextView: TextView
+    private lateinit var cheatButton: Button
 
     // This is a list of Question objects in the Question constructor
     // In more complex projects, this list would be stored elsewhere, but for now it's contained
     // in the constructor for this first app we're building.
 
-    private val quizViewModel : QuizViewModel by lazy {
+    private val quizViewModel: QuizViewModel by lazy {
         ViewModelProviders.of(this).get(QuizViewModel::class.java)
     }
 
@@ -32,13 +39,14 @@ class MainActivity : AppCompatActivity() {
         Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
 
-        val currentIndex = savedInstanceState?.getInt(KEY_INDEX, 0 )  ?: 0
+        val currentIndex = savedInstanceState?.getInt(KEY_INDEX, 0) ?: 0
         quizViewModel.currentIndex = currentIndex
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
         questionTextView = findViewById(R.id.question_text_view)
+        cheatButton = findViewById(R.id.cheat_button)
 
         trueButton.setOnClickListener { view: View ->
             checkAnswer(true)
@@ -52,8 +60,18 @@ class MainActivity : AppCompatActivity() {
             quizViewModel.moveToNext()
             updateQuestion()
         }
+        cheatButton.setOnClickListener {
+            startCheatActivity()
+        }
 
         updateQuestion()
+    }
+
+    private fun startCheatActivity() {
+        val answerIsTrue = quizViewModel.currentQuestionAnswer
+        val intent = CheatActivity.newIntent(this@MainActivity, answerIsTrue)
+//        startActivity(intent)
+        startActivityForResult(intent, REQUEST_CODE_CHEAT)
     }
 
     override fun onSaveInstanceState(savedInstanceState: Bundle) {
@@ -73,7 +91,7 @@ class MainActivity : AppCompatActivity() {
         val messageResId =
             if (userAnswer == correctAnswer) {
                 R.string.correct_toast
-            } else  {
+            } else {
                 R.string.incorrect_toast
             }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
