6edea9817c247f8c1f71c55628663c98e348d802
dan <email@email.com>
2023-02-08 16:05:29 -0600

    Added CheatActivity.kt, et al

---
+++ app/src/main/java/com/example/geoquiz/CheatActivity.kt
@@ -0,0 +1,45 @@
+package com.example.geoquiz
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+private const val EXTRA_ANSWER_IS_TRUE = "com.example.geoquiz.ANSWER_IS_TRUE"
+
+class CheatActivity : AppCompatActivity() {
+    private lateinit var answerTextView: TextView
+    private lateinit var showAnswerButton: Button
+
+    private var answerIsTrue = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cheat)
+
+        answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false)
+        answerTextView = findViewById(R.id.answer_text_view)
+        showAnswerButton = findViewById(R.id.show_answer_button)
+
+        showAnswerButton.setOnClickListener {
+            val answerText = when {
+                answerIsTrue -> R.string.true_button
+                else -> R.string.false_button
+            }
+            answerTextView.setText(answerText)
+        }
+    }
+    // A companion object allows one to access functions without having an instance of a class,
+    // similar to static functions in Java programming.
+    // This function is so coded such that it can make it easy to properly configure for new intents.
+    // The function is called in MainActivity.
+    companion object {
+        fun newIntent(packageContext: Context, answerIsTrue: Boolean): Intent {
+            return Intent(packageContext, CheatActivity::class.java).apply {
+                putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue)
+            }
+        }
+    }
+}
